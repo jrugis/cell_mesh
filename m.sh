029f08ab70733efb28db1c45d73de20ffaf23df5
jrugis <j.rugis@auckland.ac.nz>
2016-10-18 20:26:45 +1300

    mesh2vtk complete

--- m.sh
+++ m.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-g++-5 -std=c++14 -Wall -Wextra -Werror mesh2vtk.cpp -o mesh2vtk
+clang++-3.8 -std=c++14 -Wall -Wextra -Werror mesh2vtk.cpp -o mesh2vtk
 
 
--- mesh2vtk.py
+++ mesh2vtk.py
@@ -5,6 +5,7 @@ import numpy as np
 mdir = "mesh3d/"
 fname = "out_p6-p4-p8"
 
+####################
 print "input mesh data file"
 f1 = open(mdir+fname+".mesh", 'r')
 
@@ -17,35 +18,88 @@ for t in range(pcount):
 
 for line in f1: 
   if line.startswith("Triangles"): break
-tcount = int(f1.next())
-tris = np.empty((tcount, 4), dtype=np.int)
-for t in range(tcount):
+trisc = int(f1.next())
+tris = np.empty((trisc,4), dtype=int)
+for t in range(trisc): 
   tris[t] = map(int,f1.next().split())
+
+for line in f1: 
+  if line.startswith("Tetrahedra"): break
+tetsc = int(f1.next())
+tets = np.empty((tetsc,5), dtype=int)
+for t in range(tetsc):
+  tets[t] = map(int,f1.next().split())
 
 f1.close()
 
-print "output vtk data files"
-f2 = open(mdir+fname+".vtk", 'w')
-f2.write("# vtk DataFile Version 2.0\n")
-f2.write("mesh data\n")
-f2.write("ASCII\n")
-f2.write("DATASET UNSTRUCTURED_GRID\n")
+####################
+print "identify geometry"
 
-f2.write("POINTS "+str(pcount)+" float\n")
-for v in xyz:
-  f2.write(str(v[0]-35.33)+' '+str(35.33-v[1])+' '+str(11.59-v[2])+'\n')
+ftype = [('v0', np.int),('v1', np.int),('v2', np.int),('label', 'S2')]
+faces = np.empty(trisc/2, dtype=ftype)
+for i in range(len(faces)):
+  faces[i] = (tris[2*i][0],tris[2*i][1],tris[2*i][2],str(tris[2*i][3])+str(tris[2*i+1][3]))
+fl, lc = np.unique(faces['label'], return_counts=True)
 
-f2.write("CELLS "+str(tcount)+" "+str(tcount*4)+"\n")
-for v in tris:
-  f2.write("3 "+str(v[0]-1)+' '+str(v[1]-1)+' '+str(v[2]-1)+'\n')
+vtype = [('v0', np.int),('v1', np.int),('v2', np.int),('v3', np.int),('label', 'S1')]
+vols = np.empty(tetsc, dtype=vtype)
+for i in range(tetsc):
+  vols[i] = (tets[i][0],tets[i][1],tets[i][2],tets[i][3],str(tets[i][4]))
+vl, vc = np.unique(vols['label'], return_counts=True)
 
-f2.write("CELL_TYPES "+str(tcount)+"\n")
-for t in range(tcount):
-  f2.write("5 ")
-f2.write("\n")
+####################
+print "output vtk data files for faces"
 
-f2.close()
+for i, f in enumerate(fl):
+  f2 = open(mdir+fname+"_"+fl[i]+".vtk", 'w')
+  f2.write("# vtk DataFile Version 2.0\n")
+  f2.write("mesh data\n")
+  f2.write("ASCII\n")
+  f2.write("DATASET UNSTRUCTURED_GRID\n")
+
+  f2.write("POINTS "+str(pcount)+" float\n")
+  for v in xyz:
+    f2.write(str(v[0]-35.33)+' '+str(35.33-v[1])+' '+str(11.59-v[2])+'\n')
+
+  f2.write("CELLS "+str(lc[i])+" "+str(lc[i]*4)+"\n")
+  for v in faces:
+    if v[3] == f:
+      f2.write("3 "+str(v[0]-1)+' '+str(v[1]-1)+' '+str(v[2]-1)+'\n')
+
+  f2.write("CELL_TYPES "+str(lc[i])+"\n")
+  for t in range(lc[i]): f2.write("5 ")
+  f2.write("\n")
+
+  f2.close()
+
+####################
+print "output vtk data files for volumes"
+
+for i, f in enumerate(vl):
+  f2 = open(mdir+fname+"_"+vl[i]+".vtk", 'w')
+  f2.write("# vtk DataFile Version 2.0\n")
+  f2.write("mesh data\n")
+  f2.write("ASCII\n")
+  f2.write("DATASET UNSTRUCTURED_GRID\n")
+
+  f2.write("POINTS "+str(pcount)+" float\n")
+  for v in xyz:
+    f2.write(str(v[0]-35.33)+' '+str(35.33-v[1])+' '+str(11.59-v[2])+'\n')
+
+  f2.write("CELLS "+str(vc[i])+" "+str(vc[i]*5)+"\n")
+  for v in vols:
+    if v[4] == f:
+      f2.write("4 "+str(v[0]-1)+' '+str(v[1]-1)+' '+str(v[2]-1)+' '+str(v[3]-1)+'\n')
+
+  f2.write("CELL_TYPES "+str(vc[i])+"\n")
+  for t in range(vc[i]): f2.write("10 ")
+  f2.write("\n")
+
+  f2.close()
+
+####################
 
 
-print
+
+
 
