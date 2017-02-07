eda6c8423b31134d59b25ad89cbc7b90a4d6c8bd
jrugis <j.rugis@auckland.ac.nz>
2017-02-07 21:25:36 +1300

    align in matlab

--- mesh2msh.m
+++ mesh2msh.m
@@ -1,32 +1,44 @@
-% parameters
+%function [verts, nverts, tris, ntris] = mesh2msh(mdir, fname)
+
 mdir = 'mesh3d/';
 fname = 'out_p6-p4-p8';
 
-% input
+% input from file
 fid = fopen(strcat(mdir,fname,'.mesh'),'r');
 
 while ~startsWith(fgetl(fid),'Vertices')
 end
 nverts = sscanf(fgetl(fid),'%d');
-verts = transpose(single(fscanf(fid,'%f %f %f %*d',[3,nverts])));
+verts = transpose(fscanf(fid,'%f %f %f %*d',[3,nverts]));
+verts = [verts(:,1)-35.3 35.3-verts(:,2) 11.80-verts(:,3)];
 
 while ~startsWith(fgetl(fid),'Triangles')
 end
 ntris = sscanf(fgetl(fid),'%d');
-tris = transpose(int32(fscanf(fid,'%d %d %d %d',[4,ntris])));
+tris = transpose(fscanf(fid,'%d %d %d %d',[4,ntris]));
 
 fclose(fid);
 tris = unique(tris,'rows','stable'); % remove outer face duplicates
 ntris = size(tris,1);
 
-% topology
+% topology for each cell
 cells = transpose(unique(tris(:,4)));
 for cell = cells
     fprintf('  cell: %d\n',cell);
     cell_tris = tris((tris(:,4) == cell),1:3);
     ncell_tris = size(cell_tris,1);
     fprintf('    triangles: %d\n',ncell_tris);
+    hold on;
+    if cell<4
+        plot_mesh(cell_tris,verts,cell);
+    end
+    %[F,V] = stlread(sprintf('../meshes/real/blender/cell0%d.stl',cell));
+    %plot_mesh(F,V,cell+7);
 end
+
+[F,V] = stlread('../meshes/real/blender/tubes.stl');
+plot_mesh(F,V,1);
+hold off;
 
 % output
 
